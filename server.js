7f606364cc1a2bc7208d086647140681bd05fb2a
Zlatika <zlata@ellery.ru>
2020-08-05 14:58:44 +0300

    status finished. Redirecting start

--- server.js
+++ server.js
@@ -11,12 +11,19 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
       path += 'index.html';
+      res.statusCode = 200;
       break;
     case '/about':
       path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-me':
+      path += 'about.html';
+      res.statusCode = 200;
       break;
     default:
       path += '404.html';
+      res.statusCode = 404;
       break;
   }
 
@@ -27,6 +34,7 @@ const server = http.createServer((req, res) => {
       res.end()
     } else {
       // res.write(data);
+
       res.end(data);
     }
   })
