247f7314e8f08174efa91ce602820fe5f695a5cf
Zlatika <zlata@ellery.ru>
2020-08-05 15:05:18 +0300

    5th lesson

--- server.js
+++ server.js
@@ -17,9 +17,11 @@ const server = http.createServer((req, res) => {
       path += 'about.html';
       res.statusCode = 200;
       break;
+      //redirecting
     case '/about-me':
-      path += 'about.html';
-      res.statusCode = 200;
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
       break;
     default:
       path += '404.html';
